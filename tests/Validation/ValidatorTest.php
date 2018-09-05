372b704ebf9d8236165b0696e9625749b07d2437
Wolfy-J <wolfy.jd@gmail.com>
2018-09-05 16:55:05 +0300

    more tests

--- tests/Validation/ValidatorTest.php
+++ tests/Validation/ValidatorTest.php
@@ -14,8 +14,10 @@ class ValidatorTest extends BaseTest
     {
         $validator = $this->validation->validate([], []);
         $this->assertTrue($validator->isValid());
+        $this->assertSame([], $validator->getErrors());
 
         $validator = $this->validation->validate(['email' => 'user@example.com'], []);
         $this->assertTrue($validator->isValid());
+        $this->assertSame([], $validator->getErrors());
     }
 }

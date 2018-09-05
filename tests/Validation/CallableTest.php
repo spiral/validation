42e5b4ab965328e748ba441b48bd7340db178c2c
Wolfy-J <wolfy.jd@gmail.com>
2018-09-05 23:49:11 +0300

    callable tests

--- tests/Validation/CallableTest.php
+++ tests/Validation/CallableTest.php
@@ -28,4 +28,24 @@ class CallableTest extends BaseTest
             ]
         ]);
     }
+
+    public function testEmptyInArray()
+    {
+        $this->assertValid([
+            'i' => null
+        ], [
+            'i' => [
+                ['in_array', ['value', 'other']]
+            ]
+        ]);
+
+        $this->assertFail('i', [
+            'i' => null
+        ], [
+            'i' => [
+                ['notEmpty'],
+                ['in_array', ['value', 'other']]
+            ]
+        ]);
+    }
 }

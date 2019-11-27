3bcb1bbe43b3ab313df133dbbdd5980c33e140fe
Valentin V / vvval <valentin@spiralscout.com>
2019-11-27 17:45:38 +0300

    Add datetime checker

---
+++ src/Checker/DatetimeChecker.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license MIT
+ * @author  Valentin Vintsukevich (vvval)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Validation\Checker;
+
+use Spiral\Core\Container\SingletonInterface;
+use Spiral\Validation\AbstractChecker;
+
+/**
+ * @inherit-messages
+ */
+final class DatetimeChecker extends AbstractChecker implements SingletonInterface
+{
+    //Possible format mapping
+    private const MAP_FORMAT = [
+        'c' => 'Y-m-d\TH:i:sT'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public const MESSAGES = [
+        'future' => '[[Not a future date.]]',
+        'past'   => '[[Not a past date.]]',
+        'valid'  => '[[Not a valid date.]]',
+        'format' => '[[Value should match the specified date format {1}.]]',
+    ];
+
+    /**
+     * Check if date is in the future.
+     *
+     * @param int|string $value
+     * @return bool
+     */
+    public function future($value): bool
+    {
+        $date = $this->date($value);
+        $now = $this->now();
+        if ($date === null || $now === null) {
+            return false;
+        }
+
+        return $date > $now;
+    }
+
+    /**
+     * Check if date is in the past.
+     *
+     * @param int|string $value
+     * @return bool
+     */
+    public function past($value): bool
+    {
+        $date = $this->date($value);
+        $now = $this->now();
+        if ($date === null || $now === null) {
+            return false;
+        }
+
+        return $date < $now;
+    }
+
+    /**
+     * Check if date format matches the provided one.
+     *
+     * @param int|string $value
+     * @param string     $format
+     * @return bool
+     */
+    public function format($value, string $format): bool
+    {
+        if (!$this->isApplicableValue($value)) {
+            return false;
+        }
+
+        $date = \DateTime::createFromFormat(self::MAP_FORMAT[$format] ?? $format, (string)$value);
+
+        return $date !== false;
+    }
+
+    /**
+     * @param int|string $value
+     * @return bool
+     */
+    public function valid($value): bool
+    {
+        return $this->date($value) !== null;
+    }
+
+    /**
+     * @param string|int $value
+     * @return \DateTime|null
+     */
+    private function date($value): ?\DateTime
+    {
+        if (!$this->isApplicableValue($value)) {
+            return null;
+        }
+
+        try {
+            return new \DateTime(is_numeric($value) ? ('@' . (int)$value) : (string)$value);
+        } catch (\Throwable $e) {
+            //here's the fail;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param mixed $value
+     * @return bool
+     */
+    private function isApplicableValue($value): bool
+    {
+        return is_scalar($value) && !is_bool($value);
+    }
+
+    /**
+     * @return \DateTime
+     */
+    private function now(): ?\DateTime
+    {
+        try {
+            return new \DateTime('now');
+        } catch (\Throwable $e) {
+            //here's the fail;
+        }
+
+        return null;
+    }
+}
---
+++ tests/Validation/Checkers/DatetimeTest.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Validation\Checkers;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Validation\Checker\DatetimeChecker;
+
+class DatetimeTest extends TestCase
+{
+    /**
+     * @dataProvider futureProvider
+     * @param mixed $value
+     * @param bool  $expected
+     */
+    public function testFuture($value, bool $expected): void
+    {
+        $checker = new DatetimeChecker();
+
+        $this->assertSame($expected, $checker->future($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function futureProvider(): array
+    {
+        return [
+            [time() + 10, true],
+            [time() - 10, false],
+            ['', false],
+            [0, false],
+            [1.1, false],
+            ['date', false],
+            [false, false],
+            [true, false],
+            [null, false],
+            [[], false],
+            [new \stdClass(), false],
+        ];
+    }
+
+    /**
+     * @dataProvider pastProvider
+     * @param mixed $value
+     * @param bool  $expected
+     */
+    public function testPast($value, bool $expected): void
+    {
+        $checker = new DatetimeChecker();
+
+        $this->assertSame($expected, $checker->past($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function pastProvider(): array
+    {
+        return [
+            [time() - 10, true],
+            [time() + 10, false],
+            ['', true],
+            [0, true],
+            [1.1, true],
+            ['date', false],
+            [false, false],
+            [true, false],
+            [null, false],
+            [[], false],
+            [new \stdClass(), false],
+        ];
+    }
+
+    /**
+     * @dataProvider validProvider
+     * @param mixed $value
+     * @param bool  $expected
+     */
+    public function testValid($value, bool $expected): void
+    {
+        $checker = new DatetimeChecker();
+
+        $this->assertSame($expected, $checker->valid($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function validProvider(): array
+    {
+        return [
+            [time() - 10, true],
+            [time(), true],
+            [time() + 10, true],
+            ['', true],
+            [0, true],
+            [1.1, true],
+            ['date', false],
+            [false, false],
+            [true, false],
+            [null, false],
+            [[], false],
+            [new \stdClass(), false],
+        ];
+    }
+
+    /**
+     * @dataProvider formatProvider
+     * @param mixed  $value
+     * @param string $format
+     * @param bool   $expected
+     */
+    public function testFormat($value, string $format, bool $expected): void
+    {
+        $checker = new DatetimeChecker();
+
+        $this->assertSame($expected, $checker->format($value, $format));
+    }
+
+    /**
+     * @return array
+     */
+    public function formatProvider(): array
+    {
+        return [
+            ['2019-12-27T14:27:44+00:00', 'c', true], //this one is converted using other format chars
+            ['2019-12-27T14:27:44+00:00', 'Y-m-d\TH:i:sT', true], //like the 'c' one
+            ['Wed, 02 Oct 19 08:00:00 EST', \DateTime::RFC822, true],
+            ['Wed, 02 Oct 19 08:00:00 +0200', \DateTime::RFC822, true],
+            ['2019-12-12', 'Y-m-d', true],
+            ['2019-12-12', 'Y-d-m', true],
+            ['2019-13-12', 'Y-m-d', true],
+            ['2019-12-13', 'Y-d-m', true],
+            ['2019-12-Nov', 'Y-d-M', true],
+            ['2019-12-Nov', 'Y-m-\N\o\v', true],
+            ['2019-12-Nov', 'Y-M-d', false],
+            ['2019-12-Nov', '123', false],
+            ['2019+12-Nov', 'Y-m-d', false],
+            ['-2019-12-Nov', 'Y-m-d', false],
+            ['2019-12-Abc', 'Y-d-M', false],
+        ];
+    }
+}
